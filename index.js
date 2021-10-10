af809f4b78d26f0d34ac8bba3b445c5de68fe2f7
garycu <89985066+garycu@users.noreply.github.com>
2021-10-10 00:13:44 -0400

    remove images

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
+const request = require('request');
+const Storage = require('@google-cloud/storage');
 const moment = require('moment');
 const { HOST, COLLECTION_NAME } = require('./src/constants');
 const fs = require('fs');
 const PORT = process.env.PORT || 5000;
 const Web3 = require("web3");
 const MySmartContract = require("./NCC.json");
+const { response } = require('express');
 const smartContractAddress = "0x5b733f8a3209a2eabbe64eba915eb433ad508e1d";
 
 let totalSupply = 0;
@@ -26,14 +29,41 @@ const app = express()
   .set('view engine', 'ejs');
 
 // Static public files
-app.use(`/${COLLECTION_NAME}`, express.static(path.join(__dirname, 'public')));
+// app.use(`/${COLLECTION_NAME}`, express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.send('The API for CU\'s SkywalkerZ collection!');
 });
 
+app.get(`/${COLLECTION_NAME}/images/:token_id`, async function (req, res) {
+  if (parseInt(req.params.token_id) > totalSupply) {
+    console.log("total supply is less than token ID: " + totalSupply);
+    req.url = '/'
+    return app._router.handle(req, res)
+  }
+  
+  const url = 'https://storage.googleapis.com/cunft/build/images/';
+
+  const tokenId = req.params.token_id;
+
+  request({
+    url: url + tokenId,
+    encoding: null
+  }, 
+  (err, resp, buffer) => {
+    if (!err && resp.statusCode === 200){
+      res.writeHead(200, {
+        'Content-Type': 'image/png',
+        'Content-Length': resp.body.length
+      });
+      res.end(resp.body);
+    }
+  });
+});
+
 app.get(`/${COLLECTION_NAME}/:token_id`, function (req, res) {
   if (parseInt(req.params.token_id) > totalSupply) {
+    console.log(totalSupply);
     req.url = '/'
     return app._router.handle(req, res)
   }
